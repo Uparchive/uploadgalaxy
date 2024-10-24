3e705345fc6403f7fa0811ee5f7f7d93f40f427f
Uparchive <140474627+Uparchive@users.noreply.github.com>
2024-10-23 23:05:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -332,14 +332,10 @@ function playVideo(url) {
 function getMimeType(url) {
     const extension = url.split('.').pop().toLowerCase();
     switch (extension) {
-        case 'mp4':
-            return 'video/mp4';
-        case 'webm':
-            return 'video/webm';
-        case 'mkv':
-            return 'video/x-matroska';
-        default:
-            return null; // Retorna null se não for um tipo suportado
+        case 'mkv': return 'video/x-matroska';
+        case 'mp4': return 'video/mp4';
+        case 'webm': return 'video/webm';
+        default: return 'video/mp4';
     }
 }
 
