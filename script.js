21f100cd0152030510e14109c2892bd854980587
Uparchive <140474627+Uparchive@users.noreply.github.com>
2024-10-28 15:16:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -279,14 +279,14 @@ function displayFiles(files) {
         const isVideo = file.name.endsWith('.mp4') || file.name.endsWith('.mkv') || file.name.endsWith('.webm');
 
         listItem.innerHTML = `
-    <span>${file.name} (${formatBytes(file.size)})</span>
-    <div class="file-actions">
-        ${isVideo ? `<button class="play-button"><i class="fas fa-play"></i> Play</button>` : ''}
-        <a href="${file.url}" class="download-button" download="${file.name}"><i class="fas fa-download"></i> Download</a>
-        <button class="share-button"><i class="fas fa-link"></i> Link</button>
-        <button class="delete-button"><i class="fas fa-trash"></i> Excluir</button>
-    </div>
-`;
+            <span>${file.name} (${formatBytes(file.size)})</span>
+            <div class="file-actions">
+                ${isVideo ? `<button class="play-button"><i class="fas fa-play"></i> <span class="button-text">Play</span></button>` : ''}
+                <a href="${file.url}" class="download-button" download="${file.name}"><i class="fas fa-download"></i> <span class="button-text">Download</span></a>
+                <button class="share-button"><i class="fas fa-link"></i> <span class="button-text">Link</span></button>
+                <button class="delete-button"><i class="fas fa-trash"></i> <span class="button-text">Excluir</span></button>
+            </div>
+        `;
         fileList.appendChild(listItem);
 
         // Adicionar event listeners aos botões
